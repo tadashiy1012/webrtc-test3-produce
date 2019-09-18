e2c7ccc007ac3ac874d8e2ef20da7615837522b7
TadashiYamazaki <yama3ki.tada4@gmail.com>
2019-09-18 14:14:35 +0900

    2nd commit

--- main.js
+++ main.js
@@ -30,26 +30,35 @@ ws.onmessage = (ev) => {
                 (async () => {
                     await pc.setRemoteDescription(offer);
                     await pc.addIceCandidate(candidate);
+                    console.log('set remote desc');
+                    const answer = await pc.createAnswer();
+                    await pc.setLocalDescription(answer);
+                    const json = {id, answer, toId: data.id, to: 'default@890'};
+                    ws.send(JSON.stringify(json));
+                    console.log('send answer');
                 })();
             } else {
                 (async () => {
                     await pc.addIceCandidate(candidate);
-                    const answer = await pc.createAnswer()
-                    await pc.setLocalDescription(answer);
-                    const json = {id, answer, toId: data.id, to: 'default@890'};
-                    ws.send(JSON.stringify(json));
+                    console.log('add ice candidate');
                 })();
             }
         }
     }
 };
 
-function makePc(id) {
+function makePc(tgtId) {
     const pc = new RTCPeerConnection({
         iceServers: [{urls: 'stun:stun.services.mozilla.com:3478'}]
     });
     pc.onicecandidate = (ev) => {
         console.log(ev);
+        if (ev.candidate) {
+            const sdp = pc.localDescription.sdp;
+            const json = {id, toId: tgtId, candidate: ev.candidate, sdp, to: 'default@890'};
+            ws.send(JSON.stringify(json));
+            console.log('send candidate:' + tgtId);
+        }
     };
     pc.onicegatheringstatechange = (ev) => {
         console.log(ev.currentTarget.iceGatheringState)
@@ -60,3 +69,6 @@ function makePc(id) {
     pclist[id] = pc;
     return pc;
 }
+
+const idElm = document.getElementById('id');
+idElm.innerHTML = id;
